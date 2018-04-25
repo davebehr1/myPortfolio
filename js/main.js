0e1c585836fdd05a6dc1570259d779b05a468baf
AlonDavidBehr <davebehr1@gmail.com>
2018-04-25 20:12:24 +0200

    fixed modal

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@
 
   $("#formMessage").submit(function(e) {
   e.preventDefault();
-  alert('hey');
+ // $('#feedback').append('<h3 style="color:white;text-align:center;">will be in contact soon</h3>');
+  $('<h3 style="color:white;text-align:center;">will be in contact soon</h3>').hide().appendTo('#feedback').fadeIn(1000);
 
   var $form = $(this);
   $.post($form.attr("action"), $form.serialize()).then(function() {
